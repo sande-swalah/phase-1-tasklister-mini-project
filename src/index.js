4e25192c80b908ceacaa89cfaaeec5d568efb8f8
sande-swalah <alvinswalah@gmail.com>
2026-01-04 21:46:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const tasksUl = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskText = e.target['new-task-description'].value;
+    buildToDo(taskText);
+    form.reset();
+  });
+
+  function buildToDo(task) {
+    if (!task) return;
+    const li = document.createElement('li');
+    li.textContent = task;
+    tasksUl.appendChild(li);
+  }
 });
